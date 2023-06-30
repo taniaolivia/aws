39f4015de78c7de6e5fb7abc3bc1309ff16e332b
Tania Olivia <tania.olivia69@yahoo.com>
2023-06-30 23:21:49 +0200

    add html files routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const path = require('path');
 
 const hostname = "0.0.0.0";
 const port = 3001;
@@ -20,6 +21,22 @@ server.use(cors());
 const userRoute = require("./routes/userRoute");
 userRoute(server);
 
-server.use(express.static('public'));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+server.get('/inscription', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+server.get('/connexion', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+server.get('/profil', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+});
 
 server.listen(port, hostname);
